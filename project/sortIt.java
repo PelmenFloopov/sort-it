062222db328f2a4d0f7ac976680446522cc848f7
PelmenFloopov <142475020+PelmenFloopov@users.noreply.github.com>
2023-08-19 19:51:29 +0700

    Add files via upload

--- project/sortIt.java
+++ project/sortIt.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import java.io.*;
+import java.nio.file.*;
 import java.util.*;
 
 import com.google.common.collect.*;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import javax.sound.midi.Soundbank;
 
 
 public class sortIt
@@ -19,9 +21,40 @@ public class sortIt
 
     public static void main(String[] args)
     {
+        /*for (int i = 1; i < 4; i++)
+        {
+            try
+            {
+                File tmp = new File("in" + Integer.toString(i) + ".txt");
+                if (!tmp.exists())
+                {
+                    tmp.createNewFile();
+                }
+                else
+                {
+                    tmp.delete();
+                    tmp.createNewFile();
+                }
+                BufferedWriter w = new BufferedWriter(new FileWriter(tmp));
+                for (int j = 1; j <= 100_000_000; j++)
+                {
+                    w.write(Integer.toString(j * (i % 3)));
+                    w.newLine();
+                }
+                w.close();
+            }
+            catch (IOException exception)
+            {
+                System.out.println(exception.getMessage());
+            }
+        }*/
+
+        long start = System.currentTimeMillis();
+
         // Not enough parameters handling
         if (args.length < 3)
         {
+            System.out.println("Not enough arguments");
             System.out.println("Usage: java sort-it <sort_mode> <data_type> <output_file> <input_files...>");
             return;
         }
@@ -41,26 +74,20 @@ public class sortIt
         // Wrong sorting mode handling
         if (isSortingModeSelected && !sortMode.equals("-a") && !sortMode.equals("-d"))
         {
-            System.out.printf("Wrong sorting mode. Use -a for ascending and -d for descending.\n"+
+            System.out.printf("Wrong sorting mode. Use -a for ascending and -d for descending.\n" +
                     "You used " + RED + "%s\n" + RESET, sortMode);
             isCorrectParameters = false;
         }
 
         String outputFileName = isSortingModeSelected ? args[2] : args[1];
-        // Wrong output file name handling
-        if (!outputFileName.matches("^[^\\\\/?*:;{}\\[\\]<>|\"']+\\.txt$"))
-        {
-            System.out.printf("Wrong output file name. Use file with correct name with format .txt.\n" +
-                    "You used " + RED + "%s\n" + RESET, outputFileName);
-            isCorrectParameters = false;
-        }
+
 
         int inputFilesStartIndex = isSortingModeSelected ? 3 : 2;
         List<String> inputFiles = new ArrayList<>();
         inputFiles.addAll(Arrays.asList(args).subList(inputFilesStartIndex, args.length));
 
-        // Wrong input file handling
-        inputFileNamesHandling(inputFiles);
+        // Wrong input files handling
+        inputFilesHandling(inputFiles);
 
         if (isCorrectParameters)
         {
@@ -72,7 +99,39 @@ public class sortIt
                     readers.add(new BufferedReader(new FileReader(inputFile)));
                 }
 
+                Path outputPath;
+                // Incorrect file name handling
+                try
+                {
+                    outputPath = Paths.get(outputFileName);
+                }
+                catch (InvalidPathException invalidPathException)
+                {
+                    System.out.printf("Wrong output file name. Use file with correct name with .bin or .txt format.\n" +
+                            "You used " + RED + "%s\n" + RESET, outputFileName);
+                    return;
+                }
+
+                // Access violation handling
+                try
+                {
+                    boolean isWritable = Files.isWritable(outputPath);
+                    if (Files.exists(outputPath))
+                    {
+                        if (!isWritable)
+                        {
+                            System.out.println("Impossible to open output file: " + RED + UNDERLINE + outputFileName + RESET);
+                            return;
+                        }
+                    }
+                }
+                catch (SecurityException securityException)
+                {
+                    System.out.println("Impossible to open output file: " + RED + UNDERLINE + outputFileName + RESET);
+                    return;
+                }
                 BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
+
                 if (dataType.equals("-i"))
                 {
                     mergeSortIntegers(readers, writer, sortMode.equals("-a"));
@@ -93,35 +152,76 @@ public class sortIt
                 System.out.printf("""
                         Data doesn't match declaration
                         You declared data as %s, but real the data is %s
-                        """, dataType.equals("-i") ? "integer":"String", dataType.equals("-i") ? "String": "integer");
+                        """, "integer", "String");
             }
         }
+        System.out.println((System.currentTimeMillis() - start) / 1000);
     }
 
-    private static void inputFileNamesHandling(@NonNull List<String> inputFiles)
+    /**
+     * Checks correctness of the input files' names
+     *
+     * @param inputFiles
+     */
+    private static void inputFilesHandling(@NonNull List<String> inputFiles)
     {
+        @FunctionalInterface
+        interface Messageble
+        {
+            void print(int index);
+        }
+
+        Messageble message =  index->
+        {
+            if (index == 0)
+            {
+                System.out.printf("Wrong input file name. Use file with correct name with .bin or .txt format.\n" +
+                        "You used " + (RED + UNDERLINE) + "%s" + RESET + " %s\n", inputFiles.get(index), inputFiles.get(index + 1));
+                isCorrectParameters = false;
+            }
+            else if (index == inputFiles.size() - 1)
+            {
+                System.out.printf("Wrong input file name. Use file with correct name with .bin or .txt format.\n" +
+                        "You used " + "%s " + (RED + UNDERLINE) + "%s\n" + RESET, inputFiles.get(index - 1), inputFiles.get(index));
+                isCorrectParameters = false;
+            }
+            else
+            {
+                System.out.printf("Wrong input file name. Use file with correct name with .bin or .txt format.\n" +
+                        "You used " + "%s " + (RED + UNDERLINE) + "%s" + RESET + " %s\n", inputFiles.get(index - 1), inputFiles.get(index), inputFiles.get(index + 1));
+                isCorrectParameters = false;
+            }
+        };
+
         for (int i = 0; i < inputFiles.size(); i++)
         {
-            if (!inputFiles.get(i).matches("^[^\\\\/?*:;{}\\[\\]<>|\"']+\\.txt$"))
+            // Incorrect file name handling
+            try
             {
-                if (i == 0)
+                Path filePath = Paths.get(inputFiles.get(i));
+                // File absence handling
+                if (!Files.exists(filePath))
                 {
-                    System.out.printf("Wrong input file name. Use file with correct name with format .txt.\n" +
-                            "You used " + (RED + UNDERLINE) + "%s" + RESET + " %s\n", inputFiles.get(i), inputFiles.get(i + 1));
+                    System.out.println("No such file in this directory: " + RED + UNDERLINE  + inputFiles.get(i) + RESET);
+                    isCorrectParameters = false;
+                    continue;
+                }
+                // Access violation handling
+                if (!Files.isReadable(filePath))
+                {
+                    System.out.println("Can't read file: " + RED + UNDERLINE  + inputFiles.get(i) + RESET);
                     isCorrectParameters = false;
                 }
-                else if (i == inputFiles.size() - 1)
-                {
-                    System.out.printf("Wrong input file name. Use file with correct name with format .txt.\n" +
-                            "You used " + "%s " + (RED + UNDERLINE) + "%s\n" + RESET, inputFiles.get(i - 1), inputFiles.get(i));
-                    isCorrectParameters = false;
-                }
-                else
-                {
-                    System.out.printf("Wrong input file name. Use file with correct name with format .txt.\n" +
-                            "You used " + "%s " + (RED + UNDERLINE) + "%s" + RESET + " %s\n", inputFiles.get(i-1), inputFiles.get(i), inputFiles.get(i + 1));
-                    isCorrectParameters = false;
-                }
+            }
+            catch (InvalidPathException invalidPathException)
+            {
+                message.print(i);
+                isCorrectParameters = false;
+            }
+            catch (SecurityException securityException)
+            {
+                System.out.println(securityException.getMessage());
+                isCorrectParameters = false;
             }
         }
     }
@@ -129,20 +229,25 @@ public class sortIt
     /**
      * Sorts sequences of integers in files contained in "readers" and writes them into the "writer" output file
      *
-     * @param readers     is a list of the input files from where we get integers
+     * @param readers     is a list of the input files from where we get data
      * @param writer      is the output file
      * @param isAscending is the sorting mode
-     * @throws IOException
+     * @throws IOException           if it can't read data from files or write it into output file
+     * @throws NumberFormatException if you set the data type as integer, but in files you have Strings
      */
     private static void mergeSortIntegers(@NonNull List<BufferedReader> readers, @NonNull BufferedWriter writer, boolean isAscending)
             throws IOException, NumberFormatException
     {
         List<Integer> previousElements = new ArrayList<>(Collections.nCopies(readers.size(), Integer.MIN_VALUE)); // To check correctness of files sorting
-        Multimap<Integer, Integer> sorter = TreeMultimap.create();
+        Multimap<Integer, Integer> sorter = TreeMultimap.create(); // Store numbers from files as a key and indexes of the files in "readers" as list of values
+        // Reading first number from each file if it isn't empty
         for (int i = 0; i < readers.size(); i++)
         {
-            Integer currentFileElement = Integer.parseInt(readers.get(i).readLine().trim());
-            sorter.put(currentFileElement, i);
+            if (readers.get(i).ready())
+            {
+                Integer currentFileElement = Integer.parseInt(readers.get(i).readLine().trim());
+                sorter.put(currentFileElement, i);
+            }
         }
 
         // Sorts integers by ascending order and writes them into output file
@@ -157,9 +262,11 @@ public class sortIt
             previousElements.set(fileNum, lowestElement);
             sorter.remove(lowestElement, fileNum);
 
+            // If all data hadn't been read from file with the lowest element at the moment, reading another element
             if (currFile.ready())
             {
                 int newValueFromCurrFile = Integer.parseInt(currFile.readLine().trim());
+                // Checking that we got correct data. If not, taking new element until we get correct element or until file ends
                 while (newValueFromCurrFile < lowestElement)
                 {
                     if (currFile.ready())
@@ -186,15 +293,27 @@ public class sortIt
         writer.close();
     }
 
+    /**
+     * Sorts sequences of Strings in files contained in "readers" and writes them into the "writer" output file
+     *
+     * @param readers     is a list of the input files from where we get data
+     * @param writer      is the output file
+     * @param isAscending is the sorting mode
+     * @throws IOException if it can't read data from files or write it into output file
+     */
     private static void mergeSortStrings(@NonNull List<BufferedReader> readers, @NonNull BufferedWriter writer, boolean isAscending)
-            throws IOException, NumberFormatException
+            throws IOException
     {
         List<String> previousElements = new ArrayList<>(Collections.nCopies(readers.size(), "")); // To check correctness of files sorting
-        Multimap<String, Integer> sorter = TreeMultimap.create();
+        Multimap<String, Integer> sorter = TreeMultimap.create(); // Store numbers from files as a key and indexes of the files in "readers" as list of values
+        // Reading first number from each file if it isn't empty
         for (int i = 0; i < readers.size(); i++)
         {
-            String currentFileElement = readers.get(i).readLine().trim();
-            sorter.put(currentFileElement, i);
+            if (readers.get(i).ready())
+            {
+                String currentFileElement = readers.get(i).readLine().trim();
+                sorter.put(currentFileElement, i);
+            }
         }
 
         // Sorts integers by ascending order and writes them into output file
@@ -209,9 +328,11 @@ public class sortIt
             previousElements.set(fileNum, lowestElement);
             sorter.remove(lowestElement, fileNum);
 
+            // If all data hadn't been read from file with the lowest element at the moment, reading another element
             if (currFile.ready())
             {
                 String newValueFromCurrFile = currFile.readLine().trim();
+                // Checking that we got correct data. If not, taking new element until we get correct element or until file ends
                 while (newValueFromCurrFile.compareTo(lowestElement) < 0)
                 {
                     if (currFile.ready())
@@ -237,6 +358,5 @@ public class sortIt
         writer.flush();
         writer.close();
     }
-
 }
 
